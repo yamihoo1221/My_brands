b6f2426604ac93293c1810a88c13a9b3e7074e97
Suzuki Fumiya <64246528+yamihoo1221@users.noreply.github.com>
2020-06-29 15:07:23 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -119,6 +119,7 @@
 
   // モバイル（レスポンシブ）
   // アイコンクリックしてメニューを表示
+  const input = document.querySelector('input');
   const icon = document.getElementById("mobileAside");
   const menu = document.querySelector("aside");
 
@@ -134,8 +135,8 @@
       icon.style.display = "block";
       menu.style.display = "block";
       menu.animate([{ opacity: "0" }, { opacity: "1" }], 1500);
-      if(icon.checked){
-        icon.style.display = "none";
+      if(input.checked){
+        input.style.display = "none";
       } else{
     }
   }
